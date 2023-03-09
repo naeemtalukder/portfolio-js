b8bcb990dd07723c66d5652f0394708d265051e0
naeemtalukder <naeemtalukder169135@gmail.com>
2023-03-09 21:13:49 +0600

    Site resposive mobile & tab done

--- app.js
+++ app.js
@@ -31,4 +31,8 @@ window.onscroll = () => {
     let header = document.querySelector('header');
 
     header.classList.toggle('sticky', window.screenY > 100);
+
+    /*############ remove toggle icon and navbar when click navbar link ##############*/
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 };
